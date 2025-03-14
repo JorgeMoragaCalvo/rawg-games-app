1512ceddfc60c02d8e68e8bb81a2c66f1148084e
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-03-14 16:08:13 -0300

    Title and index

---
+++ src/components/common/index.js
@@ -0,0 +1,10 @@
+export { default as Banner } from "./Banner";
+export { default as Breadcrumb } from "./Breadcrumb";
+export { default as Footer } from "./Footer";
+export { default as ImagesSlider } from "./ImagesSlider";
+export { default as Navbar } from "./Navbar";
+export { default as Pagination } from "./Pagination";
+export { default as Preloader } from "./Preloader";
+export { default as StartRating } from "./StartRating";
+export { default as Tabs } from "./Tabs";
+export { default as Titler } from "./Title";
