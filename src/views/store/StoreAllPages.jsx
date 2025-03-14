51191ecda8567a7106f27a5b0cba3c50f95e1c70
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-03-14 18:02:12 -0300

    store folder and index file

---
+++ src/views/store/StoreAllPages.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StoreAllPages = () => {
+  return <StoreAllPagesWrapper></StoreAllPagesWrapper>;
+};
+
+export default StoreAllPages;
+
+const StoreAllPagesWrapper = styled.div`
+  background-color: var(--clr-violet-dark-active);
+  .sc-stores {
+    min-height: 100vh;
+    padding-top: 65px;
+  }
+`;
