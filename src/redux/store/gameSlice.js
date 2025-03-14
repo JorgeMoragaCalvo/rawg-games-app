16fb9cccb848a70855d820d80bcd086484d53c0e
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-03-14 19:01:44 -0300

    App, Routers y redux folder

---
+++ src/redux/store/gameSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const gameSlice = createSlice({
+  name: "game",
+  initialState,
+  extraReducers: {},
+  reducers: {},
+});
+
+export default gameSlice.reducer;
