95a9a2c16953eeec814cdfeec832fa9b88b6cdd3
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-03-14 17:44:21 -0300

    creator and error

---
+++ src/views/creator/CreatorAllPages.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const CreatorAllPages = () => {
+  return <CreatorAllPagesWrapper></CreatorAllPagesWrapper>;
+};
+
+export default CreatorAllPages;
+
+const CreatorAllPagesWrapper = styled.div`
+  background-color: var(--clr-violet-dark-active);
+  .sc-creators {
+    min-height: 100vh;
+    padding-up: 65px;
+  }
+`;
