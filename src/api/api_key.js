90c14670e8f6ea3f5975cdd8226dad2b9ec8c495
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-03-15 16:24:38 -0300

    lógica de manejo de estado para la acción asíncrona fetchAsyncGames

---
+++ src/api/api_key.js
@@ -0,0 +1,1 @@
+export const API_KEY = "5f4b1cb41e354a0fabafffd3358a049a";
--- src/redux/store/gameSlice.js
+++ src/redux/store/gameSlice.js
@@ -1,12 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { STATUS } from "../../utils/status";
+import { fetchAsyncGames, fetchAsyncGamesDetails } from "../utils/gameUtils";
 
-const initialState = {};
+const initialState = {
+  games: [],
+  gamesStatus: STATUS.IDLE,
+  gamesSingle: [],
+  gamesSingleStatus: STATUS.IDLE,
+  gamesDetails: [],
+};
 
 const gameSlice = createSlice({
   name: "game",
   initialState,
-  extraReducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAsyncGames.pending, (state) => {
+      state.gamesStatus = STATUS.LOADING;
+    });
+
+    builder.addCase(fetchAsyncGames.fulfilled, (state, action) => {
+      state.games = action.payload;
+      state.gamesStatus = STATUS.SUCCEEDED;
+    });
+
+    builder.addCase(fetchAsyncGames.rejected, (state) => {
+      state.gamesStatus = STATUS.FAILED;
+    });
+
+    builder.addCase(fetchAsyncGamesDetails.pending, (state) => {
+      state.gamesSingleStatus = STATUS.LOADING;
+    });
+
+    builder.addCase(fetchAsyncGamesDetails.fulfilled, (state, action) => {
+      state.gamesSingle = action.payload;
+      state.gamesSingleStatus = STATUS.SUCCEEDED;
+    });
+
+    builder.addCase(fetchAsyncGamesDetails.rejected, (state) => {
+      state.gamesSingleStatus = STATUS.FAILED;
+    });
+  },
   reducers: {},
 });
+
+//export const selectAllGames = (state) => state.game.games.results;
 
 export default gameSlice.reducer;
