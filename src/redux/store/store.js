07194e8adf5a60c5305e6847b399adf2d86c65d5
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-03-14 21:25:52 -0300

    sidebar, store Slice

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,7 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import genreReducer from "./genreSlice";
+import sidebarReducer from "./sideBarSlice";
+import gameReducer from "./gameSlice";
+import storeReducer from "./storeSlice";
+import creatorReducer from "./creatorSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    creator: creatorReducer,
+    game: gameReducer,
+    genre: genreReducer,
+    sidebar: sidebarReducer,
+    store: storeReducer,
+  },
 });
 
 export default store;
---
+++ src/redux/store/storeSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const storeSlice = createSlice({
+  name: "store",
+  initialState,
+  extraReducers: {},
+  reducers: {},
+});
+
+export default storeSlice.reducer;
